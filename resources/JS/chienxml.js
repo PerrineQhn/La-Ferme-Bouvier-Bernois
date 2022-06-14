a1e826dc64825945ebc03b035a7643b38b3ab7b7
PetX33 <p.quenn27@gmail.com>
2022-06-14 15:40:43 +0200

    first commit

---
+++ resources/JS/chienxml.js
@@ -0,0 +1,59 @@
+parser = new DOMParser();
+chienxml = parser.parseFromString(`
+<catalog>
+	<chien>
+		<lien>cheyenne.html</lien>
+		<photo>cheyenne.jpg</photo>
+		<nom>Cheyenne</nom>
+		<description>
+			<naissance>17 Juin 2020</naissance>
+			<sexe>Femelle</sexe>
+			<p>Chienne qui a donné naissance à Diabolo, l'un de nos males, elle est bien contruite, mais à malheureusement été stérilisé. Très caline, elle a également une très belle fourrure</p>
+		</description>
+	</chien>
+	
+	<chien>
+		<lien>shadow.html</lien>
+		<photo>shadow.jpg</photo>
+		<nom>Shadow</nom>
+		<description>
+			<naissance>29 Août 2020</naissance>
+			<sexe>Mâle</sexe>
+			<p>Père de Diabolo, compacte, bonne ossature. Caractère doux, très attachant, qui demande beaucoup de caresse.</p>
+		</description>
+	</chien>
+	
+	<chien>
+		<lien>saphyr.html</lien>
+		<photo>saphyr.jpg</photo>
+		<nom>Saphyr</nom>
+		<description>
+			<naissance>23 Juillet 2021</naissance>
+			<sexe>Femelle</sexe>
+			<p>Saphyr est très attachante, joueuse. Très charismatique, c'est une femelle de gros gabarit. Fourrure douce.</p>
+		</description>
+	</chien>
+	
+	<chien>
+		<lien>diabolo.html</lien>
+		<photo>diabolo.jpg</photo>
+		<nom>Diabolo</nom>
+		<description>
+			<naissance>19 Septembre 2021</naissance>
+			<sexe>Mâle</sexe>
+			<p>Fils de Shadow et Cheyenne. Chien très doux, intelligent et joueur, bien dessiné. Il a hérité de la belle fourrure de sa mère et de l'ossature de son père.</p>
+		</description>
+	</chien>
+	
+	<chien>
+		<lien>ruby.html</lien>
+		<photo>ruby.jpg</photo>
+		<nom>Ruby</nom>
+		<description>
+			<naissance>22 Septembre 2021</naissance>
+			<sexe>Femelle</sexe>
+			<p>Femelle très joueuse, très attachante, elle a le plus petit gabarit de notre élevage.</p>
+		</description>
+	</chien>
+</catalog>
+`, "text/xml");
---
+++ resources/JS/chienxsl.js
@@ -0,0 +1,33 @@
+parser = new DOMParser();
+chien = parser.parseFromString(`
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:template match="/catalog">
+	<table>
+		<xsl:for-each select="chien">
+			<tr>
+				<td class="photo">
+				<a href="{lien}">
+					<img style="width:200px; height:200px;"><xsl:attribute name="src">../resources/pictures/chiens/<xsl:value-of select="photo"/></xsl:attribute>
+					</img>
+				</a>
+				</td>
+				<td class="description" style="text-align:justify;">
+					<h4><xsl:value-of select="nom"/></h4>
+					<xsl:for-each select="description">
+							<p>
+								<u>Date de naissance</u> :   <xsl:value-of select="naissance"/>
+							</p>							
+							<p>
+								<u>Sexe</u> :  <xsl:value-of select="sexe"/>
+							</p>
+							<p>
+								<xsl:value-of select="p"/>
+							</p>	
+					</xsl:for-each>
+				</td>
+			</tr>
+		</xsl:for-each>
+	</table>
+	</xsl:template>
+</xsl:stylesheet>
+`, "text/xml");
